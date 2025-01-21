fbd1cdb4c303f3fded4458eca7d8cd95bcd1a7ed
Quocduyctvn <quocduyctvn@gmail.com>
2025-01-21 18:18:39 +0700

    Add library alpinejs

---
+++ FileManager/FileManager/Controllers/FileManagerController.cs
@@ -0,0 +1,23 @@
+﻿using FileManager.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileManager.Controllers
+{
+    [ApiController]
+    [Route("filemanager")]
+    public class FileManagerController : Controller
+    {
+        File_Manager _fm;
+
+
+        public IActionResult ExecuteCmd( [FromServices] IWebHostEnvironment env)
+        {
+			// lấy đường dẫn thư mục wwwroot 
+			var wwwroot = env.WebRootPath;
+			// nôiis chuổi để có đường dẫn tới thư mục upload 
+			var uploadPath = Path.Combine(wwwroot, "upload");
+			_fm = new File_Manager(uploadPath, Request);
+			return Ok(_fm.ExecuteCmd());
+        }
+    }
+}
